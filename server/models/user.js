7677e9351f690d22e31a4e4f6092ed850677411c
rob <rgnorthrop@gmail.com>
2018-01-23 09:19:14 -0500

    xxx

--- server/models/user.js
+++ server/models/user.js
@@ -69,6 +69,18 @@ UserSchema.methods.generateAuthToken = function () {
     });
 }
 
+UserSchema.methods.removeToken = function (token) {
+    var user = this;
+
+    return user.update({
+        $pull: {
+            tokens: {
+                token: token
+            }
+        }
+    });
+}
+
 // statics makes it a model method
 UserSchema.statics.findByToken = function (token) {
     var User = this;
